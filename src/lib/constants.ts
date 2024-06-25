167f348c92a6b6a9052d04076e22387002a58216
Ronjar <robingebert99@gmail.com>
2024-06-25 21:04:21 +0200

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:80"; //Test
-//export const BASE_URL = "https://gender.robingebert.com"; //Production
-export const VERSION = "2.0.7"
+//export const BASE_URL = "http://localhost:80"; //Test
+export const BASE_URL = "https://gender.robingebert.com"; //Production
+export const VERSION = "2.0.8"
